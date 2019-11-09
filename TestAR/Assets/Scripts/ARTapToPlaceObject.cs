c04da9eb7394acfe31c1a424eab23fd3281d718b
DanielSwieca <danielswieca@gmail.com>
2019-11-09 14:42:10 -0500

    Plane Detection in AR working

--- TestAR/Assets/Scripts/ARTapToPlaceObject.cs
+++ TestAR/Assets/Scripts/ARTapToPlaceObject.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.Experimental.XR;
 using System;
+using TMPro;
 
 public class ARTapToPlaceObject : MonoBehaviour
 {
     [SerializeField] GameObject placementIndicator;
+    [SerializeField] TextMeshProUGUI proUGUI;
 
     private ARSessionOrigin arOrigin;
     private ARRaycastManager arRaycastManager;
@@ -35,20 +37,33 @@ public class ARTapToPlaceObject : MonoBehaviour
             placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
         } else
         {
-            placementIndicator.SetActive(false);
+            placementIndicator.SetActive(true);
         }
+
+        proUGUI.text = "placementPoseIsValid: " + placementPoseIsValid;
     }
 
     private void UpdatePlacementPose()
     {
-        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-        var hits = new List<ARRaycastHit>();
-        arRaycastManager.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+        try { 
+            var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+            var hits = new List<ARRaycastHit>();
+            arRaycastManager.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
 
-        placementPoseIsValid = hits.Count > 0;
-        if (placementPoseIsValid)
+
+            placementPoseIsValid = hits.Count > 0;
+            if (placementPoseIsValid)
+            {
+                Debug.Log("Found Plane!");
+                placementPose = hits[0].pose;
+            } else
+            {
+                Debug.Log("No Plane!");
+            }
+            Debug.Log("hits.Count: " + hits.Count);
+        } catch(Exception e)
         {
-            placementPose = hits[0].pose;
+            Debug.Log("Raycast error: " + e.Message);
         }
     }
 }
